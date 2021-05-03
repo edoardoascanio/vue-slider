6039a060a94d55503d708a0787a2fdbcb6d0441e
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-04 01:25:51 +0200

    correzioni

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const app = new Vue({
 
-    el:"#app",
-    data:{
-        images:[{}],
-        activeImage: "",
-    }
-}
+    el: "#app",
+    data: {
+        imagesList: [
+            "https//:picsum.photos/1080/600?i=0",
+            "https//:picsum.photos/1080/600?i=1",
+            "https//:picsum.photos/1080/600?i=2",
+            "https//:picsum.photos/1080/600?i=3",
+            "https//:picsum.photos/1080/600?i=4"
+        ],
+        activeImg: 0
+    },
+    methods:{
+        changeImg(direction){
+            const newIndex = this.activeImg + direction;
 
-)
+            if(newIndex < 0){
+                newIndex = this.imagesList.length -1;
+            }else if (newIndex >(this.imagesList.length -1)){
+
+                newIndex = 0;
+            }
+
+
+            this.activeImg += direction;
+        }
+    }
+}) 
+
