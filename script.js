05018f2532ce08d56dc8b4683cccfc4b6a611006
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-05-04 13:40:45 +0200

    add dots

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const app = new Vue({
     el: "#app",
     data: {
         imagesList: [
-            "https//:picsum.photos/1080/600?i=0",
-            "https//:picsum.photos/1080/600?i=1",
-            "https//:picsum.photos/1080/600?i=2",
-            "https//:picsum.photos/1080/600?i=3",
-            "https//:picsum.photos/1080/600?i=4"
+            "https://picsum.photos/1080/600?i=0",
+            "https://picsum.photos/1080/600?i=1",
+            "https://picsum.photos/1080/600?i=2",
+            "https://picsum.photos/1080/600?i=3",
+            "https://picsum.photos/1080/600?i=4"
         ],
         activeImg: 0
     },
-    methods:{
-        changeImg(direction){
+    methods: {
+        /**
+         * 
+         * @param {number} direction 
+         */
+        changeImg(direction) {
             const newIndex = this.activeImg + direction;
 
-            if(newIndex < 0){
-                newIndex = this.imagesList.length -1;
-            }else if (newIndex >(this.imagesList.length -1)){
+            if (newIndex < 0) {
+                newIndex = this.imagesList.length - 1;
+            } else if (newIndex > (this.imagesList.length - 1)) {
 
                 newIndex = 0;
             }
@@ -26,5 +30,5 @@ const app = new Vue({
             this.activeImg += direction;
         }
     }
-}) 
+})
 
